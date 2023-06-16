c979cc8f383357cc39f3dfb4d16513525a8db98d
STINGO <32518650+Stinnngo@users.noreply.github.com>
2023-06-17 04:28:34 +0800

    更新 TermuxApplication.java

--- app/src/main/java/com/termux/app/TermuxApplication.java
+++ app/src/main/java/com/termux/app/TermuxApplication.java
@@ -2,6 +2,7 @@ package com.termux.app;
 
 import android.app.Application;
 import android.content.Context;
+import androidx.multidex.MultiDexApplication
 
 import com.termux.BuildConfig;
 import com.termux.shared.errors.Error;
