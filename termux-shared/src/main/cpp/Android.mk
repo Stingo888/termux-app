49886612d7529d2cee0bca474c0da8b435b8c910
STINGO <32518650+Stinnngo@users.noreply.github.com>
2023-06-25 16:56:28 +0800

    更新 Android.mk

--- termux-shared/src/main/cpp/Android.mk
+++ termux-shared/src/main/cpp/Android.mk
@@ -1,6 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_LDLIBS := -llog
 LOCAL_MODULE := local-socket
 LOCAL_SRC_FILES := local-socket.cpp
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := readlink
+LOCAL_SRC_FILES := readlink.cpp
+include $(BUILD_STATIC_LIBRARY)
+
