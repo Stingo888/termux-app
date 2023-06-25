9921bf5aff490139a672a4986e0f37448ca0147a
STINGO <32518650+Stinnngo@users.noreply.github.com>
2023-06-25 17:46:31 +0800

    Add files via upload

---
+++ termux-shared/src/main/cpp/file-utils.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+#include "scoped_utf_chars.h"
+#include "readlink.h"
+#include "file-utils.h"
+
+JNIEXPORT jstring JNICALL Java_FileUtils_readlink
+  (JNIEnv *env, jclass, jstring javaPath) {
+
+    ScopedUtfChars path(env, javaPath);
+    if (path.c_str() == NULL) { return NULL; }
+
+    std::string result;
+    if (!readlink(path.c_str(), result)) { return NULL; }
+
+    return env->NewStringUTF(result.c_str());
+}
+
+JNIEXPORT void JNICALL Java_FileUtils_symlink
+  (JNIEnv *env, jclass, jstring javaOldPath, jstring javaNewPath) {
+
+    ScopedUtfChars oldPath(env, javaOldPath);
+    if (oldPath.c_str() == NULL) { return; }
+
+    ScopedUtfChars newPath(env, javaNewPath);
+    if (newPath.c_str() == NULL) { return; }
+
+    symlink(oldPath.c_str(), newPath.c_str());
+}
