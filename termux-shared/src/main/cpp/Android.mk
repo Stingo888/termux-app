8648c5e4e11581d8bb99adf564b6590d6c1cb4e5
STINGO <32518650+Stinnngo@users.noreply.github.com>
2023-07-04 08:09:20 +0800

    Update Android.mk

--- termux-shared/src/main/cpp/Android.mk
+++ termux-shared/src/main/cpp/Android.mk
@@ -12,8 +12,8 @@ LOCAL_SRC_FILES := readlink.cpp
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := file-utils
-LOCAL_SRC_FILES := file-utils.cpp
+LOCAL_MODULE := posix
+LOCAL_SRC_FILES := posix.cpp
 LOCAL_STATIC_LIBRARIES := libreadlink
 include $(BUILD_SHARED_LIBRARY)
 
