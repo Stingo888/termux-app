6667e9699b94a1756b783b572b69d22689401c0e
STINGO <32518650+Stinnngo@users.noreply.github.com>
2023-07-11 13:46:00 +0800

    Add files via upload

--- termux-shared/src/main/java/com/termux/shared/file/libcore/NativeConverter.java
+++ termux-shared/src/main/java/com/termux/shared/file/libcore/NativeConverter.java
@@ -1,5 +1,5 @@
 package com.termux.shared.file.libcore;
-
+import com.termux.shared.nio.charset.Charset;
 public class NativeConverter {
     public static native Charset charsetForName(String charsetName);
     static { System.loadLibrary("nativeconverter"); }
